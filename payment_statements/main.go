24679024b01a7ae83d66d06d0c49b278457b0d7f
Preslav Mihaylov <preslav@uber.com>
2020-05-25 14:22:49 +0300

    extracted grpc server creation of payment_statements outside of main

--- payment_statements/main.go
+++ payment_statements/main.go
@@ -1,11 +1,24 @@
 package main
 
+import (
+	"net"
+
+	"google.golang.org/grpc"
+	// commonpb "gitlab.com/preslavmihaylov/go-comm-protocols-exercise/gen/common"
+	// "gitlab.com/preslavmihaylov/go-comm-protocols-exercise/gen/payment_statements"
+)
+
 var (
 	paymentStatementsAddr = "localhost:10001"
 )
 
 func main() {
-	// TODO: Create gRPC server
+	grpcServer, lis := setupPaymentStatementsServer()
+	grpcServer.Serve(lis)
+}
+
+func setupPaymentStatementsServer() (*grpc.Server, net.Listener) {
+	panic("not implemented")
 }
 
 type server struct{}
