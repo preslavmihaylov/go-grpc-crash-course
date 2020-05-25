6fada9274a22d2c2f6954942c73846f787acd0b4
Preslav Mihaylov <preslav@uber.com>
2020-05-25 14:40:33 +0300

    implement make_protos.sh

--- make_protos.sh
+++ make_protos.sh
@@ -4,4 +4,6 @@ mkdir -p gen/common
 mkdir -p gen/casino
 mkdir -p gen/payment_statements
 
-# TODO: Generate protobuf sources
+protoc -I idl idl/common/types.proto --go_out=paths=source_relative:gen/
+protoc -I idl idl/casino/casino.proto --go_out=plugins=grpc:gen
+protoc -I idl idl/payment_statements/payment_statements.proto --go_out=plugins=grpc:gen
