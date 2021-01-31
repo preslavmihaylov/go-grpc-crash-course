ca6235ad367121d72d5f14459cd05b62d9d3e68b
preslavmihaylov <preslav@pmihaylov.com>
2021-01-31 16:24:39 +0200

    update proto creation script

--- make_protos.sh
+++ make_protos.sh
@@ -4,6 +4,13 @@ mkdir -p gen/common
 mkdir -p gen/casino
 mkdir -p gen/payment_statements
 
-protoc -I idl idl/common/types.proto --go_out=paths=source_relative:gen/
-protoc -I idl idl/casino/casino.proto --go_out=plugins=grpc:gen
-protoc -I idl idl/payment_statements/payment_statements.proto --go_out=plugins=grpc:gen
+protoc -I idl idl/common/types.proto \
+    --go_out=gen --go-grpc_out=require_unimplemented_servers=false:gen \
+    --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
+protoc -I idl idl/casino/casino.proto \
+    --go_out=gen --go-grpc_out=require_unimplemented_servers=false:gen \
+    --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
+protoc -I idl idl/payment_statements/payment_statements.proto \
+    --go_out=gen --go-grpc_out=require_unimplemented_servers=false:gen \
+    --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
+
